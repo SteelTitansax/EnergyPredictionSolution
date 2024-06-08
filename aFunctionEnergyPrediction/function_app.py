1913d24764c7fc4f888c9d60dad09cf81a4a6023
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2024-06-08 18:16:15 +0200

    Add files via upload

--- aFunctionEnergyPrediction/function_app.py
+++ aFunctionEnergyPrediction/function_app.py
@@ -10,12 +10,13 @@ def aFunctionEnergyPrediction(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Starting prediction ....')
 
+
     try:
         req_body = req.get_json()
     except ValueError:
         pass
     else:
-        Usage_Kw = req_body.get('Usage_kwh')
+
         Lagging_Current_Reactive_Power_KvarH = req_body.get('Lagging_Current_Reactive_Power_KvarH')
         Leading_Current_Reactive_Power_kVarh = req.body.get('Leading_Current_Reactive_Power_kVarh')
         CO2_tCo2 = req.body.get('CO2_tCo2')
@@ -24,21 +25,19 @@ def aFunctionEnergyPrediction(req: func.HttpRequest) -> func.HttpResponse:
         NSM = req.body.get('NSM')
         WeekStatus = req.body.get(WeekStatus)
         Day_of_week = req.body.get('Day_of_week')
-        
+        Load_Type = req.body.get('Load_Type')        
+    
+        X_test = [Lagging_Current_Reactive_Power_KvarH, Leading_Current_Reactive_Power_kVarh, CO2_tCo2, logging_Current_Power_Factor, Leading_Current_Power_Factor, NSM, WeekStatus, Day_of_week, Load_Type]
 
-        X_test = [str(datetime.now(datetime.UTC)),Usage_Kw, Lagging_Current_Reactive_Power_KvarH, Leading_Current_Reactive_Power_kVarh, CO2_tCo2, logging_Current_Power_Factor, Leading_Current_Power_Factor, NSM, WeekStatus, Day_of_week, Load_Type]
-        
         # Loading model 
         
         model_pkl_file = "./energyPredictorModel.pkl"
-        
-        
+                
         with open(model_pkl_file, 'rb') as file : 
             model = pickle.load(file)   
         
-        Load_Type =  model.predict([X_test])
-        
-        logging.info('Prediction finished successfully !!!')
-        return func.HttpResponse(Load_Type, status_code=200)
+        Usage_Kw =  model.predict([X_test])
+        logging.info(f"Usage_Kw : {Usage_Kw}")
+        return func.HttpResponse(str(Usage_Kw), status_code=200)
 
     
